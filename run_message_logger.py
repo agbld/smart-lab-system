4e54524734953a10a912d5e66909a2582f3ecdba
agbld <agiblida@gmai.com>
2024-06-02 21:11:31 +0800

    create message_logger_agent for debug from test

---
+++ run_message_logger.py
@@ -0,0 +1,32 @@
+from agent import Agent
+import hardware
+
+class MessageLoggerAgent(Agent):
+    def __init__(self, name: str, port: int = 5000, recipients: list = [], state: dict = {}, seat_id_to_rgb_pin: dict = {}):
+        super().__init__(name, port, recipients, state, seat_id_to_rgb_pin)
+        
+    def handle_message(self, message: dict):
+        # write the received message to file
+        with open(f'.received_messages_{self.name}.txt', 'a') as f:
+            f.write(f"{message}\n")
+
+state = {
+    "member": [
+        {
+            "name": "agbld",
+            "status": 0,
+            "seat_id": 0,
+        },
+        {
+            "name": "shawn ",
+            "status": 0,
+            "seat_id": 1,
+        }
+    ]
+}
+
+recipients = [
+    # {'ip_address': '127.0.0.1', 'port': 5000},
+]
+
+test_agent_2 = MessageLoggerAgent('agenet_2', port=5000, recipients=recipients, state=state, seat_id_to_rgb_pin=hardware.SEAT_ID_TO_RGB_PIN)
