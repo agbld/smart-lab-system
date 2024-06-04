20ff3cb8969da313b9ecedf6e5e4caf436aa369e
agbld <agiblida@gmai.com>
2024-06-04 14:57:33 +0800

    feat: Update IndoorFaceRecAgent initialization with recheck_counts parameter

--- run_indoor_face_rec_agent.py
+++ run_indoor_face_rec_agent.py
@@ -1,7 +1,7 @@
 import agent
 import hardware
 
-indoor_face_rec_agent = agent.IndoorFaceRecAgent('./known_faces', recipients=hardware.recipients, port=5000, state=hardware.state, brightness_threshold = 50, humidity_threshold = 50, temperature_threshold = 25)
+indoor_face_rec_agent = agent.IndoorFaceRecAgent('./known_faces', recipients=hardware.recipients, port=5000, state=hardware.state, recheck_counts=1, brightness_threshold = 50, humidity_threshold = 50, temperature_threshold = 25)
 
 # indoor_face_rec_agent.make_known_faces_embeddings()
 
